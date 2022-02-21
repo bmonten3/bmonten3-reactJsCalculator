5e68c4e203e688be3fb8fa99d727d1e78384562c
bmonten3 <77298919+bmonten3@users.noreply.github.com>
2022-02-20 23:44:29 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+//dependenices 
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //functionality of the calculator 
+    const [calc, setCalc] = useState("")
+    const [result, setResult] = useState("")
+
+    //string array of operators for functionality in later function
+    const ops = ['/', '*', '+', "-", '.'];
+
+    //adjusting the value in the results div
+    const updateCalc = value => {
+        
+        //operators troubleshooting
+        if (
+            ops.includes(value) && calc === '' ||
+            ops.includes(value) && ops.includes(calc.slice(-1))
+        
+        )
+         {
+            return;
+        }
+        setCalc(calc + value);
+
+        if (!ops.includes(value)) {
+                setResult(eval(calc + value).toString());
+        }
+        
+
+    }
+
+
+    //creating digits function 
+
+    const digitArray = () => {
+        const digits = [];
+
+        for (let i = 1; i < 10; i++) {
+            digits.push(
+                <button 
+                onClick={() => updateCalc(i.toString())}
+                key={i}>
+                    {i}
+                     
+                     </button>
+            )
+
+        }
+        return digits;
+    }
+        //equals '=' functionality
+        const calculate = () => {
+            setCalc(eval(calc).toString());
+
+        }
+
+        //DEL button functionality to delete last string in the display caluculation
+        const deleteLast = () => {
+           
+            if(calc == '') {
+                return;
+            }
+            
+           
+            const value = calc.slice(0, -1);
+
+            setCalc(value);
+        }
+  
+    return (
+        <div className="App">
+            <div className='calculator'>
+                <div className='display'>
+                    
+                    { result ? <span>({result})</span>  : ''}&nbsp;
+                    { calc || "0"}
+                </div>
+
+                <div className='operators'>
+                    <button onClick={() => updateCalc('/')}>/</button>
+                    <button onClick={() => updateCalc('*')}>*</button>
+                    <button onClick={() => updateCalc('+')}>+</button>
+                    <button onClick={() => updateCalc('-')}>-</button>
+                   
+
+                   <button onClick={deleteLast}>DEL</button>
+                  
+                </div>
+
+                <div className="digits">
+                    { digitArray() }
+                    <button onClick={() => updateCalc('0')}>0</button>
+                    <button onClick={() => updateCalc('.')}>.</button>
+
+                    
+                    <button onClick={calculate}>=</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App; 
